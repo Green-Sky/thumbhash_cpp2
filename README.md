0c840d748756c6205b2df524ec62020a4c23b36f
Green Sky <green@g-s.xyz>
2024-08-23 10:56:24 +0200

    add test data and cpp1 export and test

---
+++ README.md
@@ -0,0 +1,4 @@
+# CPP2 (cppfront) implementation of thumbhash
+
+This is mostly me just trying out cpp2 on a small example.
+
--- cpp1_test.cpp
+++ cpp1_test.cpp
@@ -1,5 +1,24 @@
-#include "thumbhash.h"
+#include "./thumbhash.h"
+
+#include "./test_data/testpic_cat1.c.inl"
+
+#include <cassert>
+#include <iomanip>
 
 int main(void) {
+	assert(testpic_cat1.bytes_per_pixel == 4);
+
+	auto resized = thumbhash::scale_to_size_nn(testpic_cat1.pixel_data, testpic_cat1.width, testpic_cat1.height);
+
+	auto hash_result = thumbhash::rgba_to_hash(resized.out_img, resized.out_w, resized.out_h);
+
+	std::cout << "computed hash:";
+	for (const auto e : hash_result) {
+		std::cout << " " << std::hex << std::setfill('0') << std::setw(2) << (int)e;
+	}
+	std::cout << "\n";
+
+	std::cout << "reconstructed aspect ratio: " << thumbhash::hash_to_aspect_ratio(hash_result) << "\n";
+
 	return 0;
 }
